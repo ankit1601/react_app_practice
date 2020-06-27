175d4eccd0783c780c27fbebccb7baa7123e1cc2
Ankit <ankit1601@gmail.com>
2020-06-27 21:12:19 +0530

    Using Eject Command unlocked setup files and now enabled css loader modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -98,17 +98,17 @@ class App extends Component {
      style.backgroundColor ='red';
    }
 
-  let classes = [];
+  let assignedClasses = [];
   if(this.state.persons.length<=2){
-    classes.push('red')
+    assignedClasses.push(classes.red)
   }
   if(this.state.persons.length<=1){
-    classes.push('bold')
+    assignedClasses.push(classes.bold)
   }
   return (
-      <div className="App">
+      <div className={classes.App}>
         <h1 key={"hdsgksjdh"} >Hi, I am React App</h1>
-        <p key={"sdnhdj"} className={classes.join(' ')}>This is really working</p>
+        <p key={"sdnhdj"} className={assignedClasses.join(' ')}>This is really working</p>
         <button key={'shdgshad'} style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
         <button key={'shdgssjdad'} style={style} onClick={()=>{return this.switchNameHandler("Ankit Aggarwal!!!")}}>Switch Name</button>
         {persons}

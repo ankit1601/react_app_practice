5619c5de1f11b329a2ae1349ce43ca328c10fa63
Ankit <ankit1601@gmail.com>
2020-07-01 01:46:38 +0530

    Tried HOC Component return class

--- react-complete-guide/src/hoc/withFuncClass.js
+++ react-complete-guide/src/hoc/withFuncClass.js
@@ -4,14 +4,29 @@
 // first try without pass props in Wrapped component
 // one beauty is we will use spread operator and just pass existing props
 
-import React from  'react';
+import React, {Component} from  'react';
+
+// const withFunClass = (WrappedComponent, className) => {
+//     return (props) => {
+//             return (<div className = {className}>
+//                 <WrappedComponent {...props}/>
+//             </div>)
+//         }
+// }
 
 const withFunClass = (WrappedComponent, className) => {
-    return (props) => {
-            return (<div className = {className}>
-                <WrappedComponent {...props}/>
-            </div>)
+    return class extends Component {
+        render(){
+            return(
+                (<div className = {className}>
+                    <WrappedComponent {...this.props}/>
+                </div>)
+            )
         }
+    } 
 }
+
+// this is stateless component but we can also return a stateful component say if you want to do somethiing in lifecycle hooks
+// notice one thing here is we don't give any name to class it is ananomous class and we use this keyword to access props
 
 export default withFunClass;
